30c9a431594ad95e418a0df5b6ce65e856e6c620
Avi <93165148+avi4h@users.noreply.github.com>
2024-09-04 16:24:20 +0530

    update heading

--- src/Top.jsx
+++ src/Top.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Top(props) {
     return (
         <div className="top" >
-            <h1>Trivia Challenge</h1>
+            <h1>Trivia Challenge R{props.rounds}</h1>
             <h2>Current Score: {props.score}</h2>
             <h3>Current Round: {props.rounds}</h3>
         </div>
